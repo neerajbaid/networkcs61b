ef08c74b8e137d892e8d0d2a4f77f176065ef6ef
Neeraj Baid <wdaareg@gmail.com>
2014-04-01 20:04:50 -0700

    Commented Chain.

--- player/Chain.java
+++ player/Chain.java
@@ -5,7 +5,10 @@ import dict.*;
 
 public class Chain
 {
+  //The player to whom this network belongs.
   public int color;
+
+  //All the pieces in this network.
   public DList pieces;
 
   public Chain(int color) {
@@ -13,31 +16,49 @@ public class Chain
     this.color = color;
   }
 
+  /**
+    * Checks if this network contains piece.
+    */
   public boolean contains(Piece piece)
   {
-    return pieces.contains(piece); //fast enumeration here skips first piece
+    return pieces.contains(piece);
   }
 
+  /**
+    * Adds piece to this network.
+    */
   public void addPiece(Piece piece)
   {
     pieces.insertBack(piece);
   }
 
+  /**
+    * Returns the number of pieces in this network.
+    */
   public int numPieces()
   {
     return pieces.length();
   }
 
+  /**
+    * Returns all the pieces in this network.
+    */
   public DList getPieces(){
     return pieces;
   }
 
+  /**
+    * Returns a copy of this network.
+    */
   public Chain copy() {
     Chain copy = new Chain(color);
     copy.pieces = pieces.copy();
     return copy;
   }
 
+  /**
+    * Returns the first piece of this network.
+    */
   public Piece first() {
     ListNode node = pieces.front();
     if (node.isValidNode()) {
